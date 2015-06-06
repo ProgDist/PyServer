071afa5278880e33354d14dcf856fd0f06924338
PedroVictorB <pedrovictor_caldas@hotmail.com>
2015-06-05 22:35:16 -0300

    Bug GET/POST retirado

--- main.py
+++ main.py
@@ -10,15 +10,26 @@ def hello_world():
 
 @app.route('/dados', methods=['GET', 'POST'])
 def dados():
-    p = Peixe(
-        temperatura = int(request.args.get('temperatura')),
-        ph = int(request.args.get('ph')),
-        dureza = int(request.args.get('dureza')),
-        alcalinidade = int(request.args.get('alcalinidade')),
-        nivelo2 = int(request.args.get('nivelo2')),
-        transparencia = int(request.args.get('transparencia')),
-    )
-    p.put()
+    if request.method == 'POST':
+        p = Peixe(
+            temperatura = int(request.form.get('temperatura')),
+            ph = int(request.form.get('ph')),
+            dureza = int(request.form.get('dureza')),
+            alcalinidade = int(request.form.get('alcalinidade')),
+            nivelo2 = int(request.form.get('nivelo2')),
+            transparencia = int(request.form.get('transparencia')),
+        )
+        p.put()
+    if request.method == 'GET':
+        p = Peixe(
+            temperatura = int(request.args.get('temperatura')),
+            ph = int(request.args.get('ph')),
+            dureza = int(request.args.get('dureza')),
+            alcalinidade = int(request.args.get('alcalinidade')),
+            nivelo2 = int(request.args.get('nivelo2')),
+            transparencia = int(request.args.get('transparencia')),
+        )
+        p.put()
     return 'ok'
 
 if __name__ == '__main__':
